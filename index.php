bb8bc3b48ecc0ba6a6b028460ae6f89013c1a1e9
Rehmat <rehmat@5683.me>
2025-12-28 16:37:45 +0530

    removed prefilled login

--- index.php
+++ index.php
@@ -254,12 +254,12 @@
                         <form onsubmit="event.preventDefault(); login(this.username.value, this.password.value);">
                             <div class="mb-4">
                                 <label class="block text-gray-700 mb-2">Username</label>
-                                <input type="text" name="username" value="admin" 
+                                <input type="text" name="username"
                                     class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500">
                             </div>
                             <div class="mb-6">
                                 <label class="block text-gray-700 mb-2">Password</label>
-                                <input type="password" name="password" value="admin-my-pm2"
+                                <input type="password" name="password"
                                     class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500">
                             </div>
                             <button type="submit" 
